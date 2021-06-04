40d8fd2589954e302f53f6071c91a722da61f0b9
Co9xs <ryo.fjsm0203@icloud.com>
2021-06-04 13:37:00 +0900

    add sample component

---
+++ src/components/sample.tsx
@@ -0,0 +1,9 @@
+import * as React from 'react'
+
+type Props = {}
+
+export const Sample: React.VFC<Props> = () => {
+  return (
+    <h2>This is Sample Component</h2>
+  )
+}
--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,13 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
+import { Sample } from './components/sample';
 
 class App extends React.Component {
   render() {
     return (
         <div>
-          Hello, React + TypeScript Starter Kit👋
+          <h1>Hello, React + TypeScript Starter Kit👋</h1>
+          <Sample/>
         </div>
     );
   }
